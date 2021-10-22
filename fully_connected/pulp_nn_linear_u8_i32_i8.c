631f801aa49aa1fcb7818d43b10fff09094d73ed
Manuele Rusci <manuele.rusci@unibo.it>
2021-10-22 19:36:57 +0200

    added fully connected

---
+++ fully_connected/pulp_nn_linear_u8_i32_i8.c
@@ -0,0 +1,84 @@
+/*
+ * pulp_nn_linear_u8_i32_i8.c
+ * Nazareno Bruschi <nazareno.bruschi@unibo.it>
+ *
+ * Copyright (C) 2019-2020 University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pmsis.h"
+#include "pulp_nn_utils.h"
+
+
+void pulp_nn_linear_u8_i32_i8(
+                  uint8_t *pIn,
+                  int8_t *pBias,
+                  int32_t *pOut,
+                  int8_t *pWeight,
+                  uint16_t dim_vec,
+                  uint16_t num_o_neurons)
+{
+
+  int core_id = pi_core_id();
+  int Log2Core = log2(NUM_CORES);
+
+  // parallelize over output feature dimension
+  int chunk = (num_o_neurons >> Log2Core) + ((num_o_neurons & (NUM_CORES-1))!=0);
+  int start = min(chunk * core_id, num_o_neurons);
+  int stop = min(start + chunk, num_o_neurons);
+
+  v4u vecA;
+  v4s vecB;
+
+  // every core make a copy and increase the output address
+  pOut = (int32_t *) pOut + start;
+
+  // iterate over the output feature space
+  for(int i=start; i<stop; i++)
+  {
+    int sum = 0;
+
+    if (pBias != NULL)
+    {
+      sum = ((int)(pBias[i]));
+    }
+
+    uint8_t *pA = pIn; // pA points to the input vector
+    int8_t *pB = pWeight + (i * dim_vec); // pB points to weight tensor
+
+    // compute the vectorized dot products
+    for (int j=0; j<(dim_vec >> 2); j++) {
+      vecA = *((v4u*)pA);
+      vecB = *((v4s*)pB);
+      sum = SumDotp4(vecA, vecB, sum);
+      pA+=4;
+      pB+=4;
+    }
+
+    // left over: handling the remaining input features 
+    uint16_t col_cnt = dim_vec & 0x3;
+    while (col_cnt)
+    {
+      uint8_t inA = *pA;
+      pA++;
+      int8_t inB = *pB;
+      pB++;
+      sum += inA * inB;
+      col_cnt--;
+    }
+    *pOut = sum;
+    pOut++;
+  }
+  pi_cl_team_barrier(0);
+}
---
+++ fully_connected/test.c
@@ -0,0 +1,131 @@
+/*
+ * test.c
+ * Nazareno Bruschi <nazareno.bruschi@unibo.it>
+ *
+ * Copyright (C) 2019-2020 University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pmsis.h"
+#include "pulp_nn_kernels.h"
+#include "pulp_nn_utils.h"
+
+
+
+// data allocation and golden models
+#include "GoldenModelLinearNoQuant/golden_8_32_8.h"
+#include "DataAllocationLinearNoQuant/data_allocation_8_32_8.h"
+
+
+
+void test();
+void pulp_parallel();
+
+void pulp_parallel()
+{
+  pi_cl_team_fork(NUM_CORES, (void *)test, NULL);
+}
+
+void test()
+{
+  // initialize checksum 
+  uint32_t errors = 0;
+
+
+  // copy inputs and weights from L2 to L1
+  if(pi_core_id()==0)
+  {
+    for(int i=0; i<(DIM_IM_IN_X * DIM_IM_IN_Y * CH_IM_IN); i++)
+    {
+      IN_INT8_L1[i] = IN_INT8_L2[i];
+    }
+
+    for(int i=0; i<(DIM_IM_IN_X * DIM_IM_IN_Y * CH_IM_IN * CH_IM_OUT); i++)
+    {
+      WEIGHT_INT8_L1[i] = WEIGHT_INT8_L2[i];
+    }
+  }
+  pi_cl_team_barrier(0);
+
+  // setup and start performance counters
+  pi_perf_conf(1<<PI_PERF_CYCLES);          
+  pi_perf_reset();                      
+  pi_perf_stop();                       
+  pi_perf_start(); 
+
+  // call the fully connected
+  pulp_nn_linear_u8_i32_i8(
+    IN_INT8_L1,
+    BIAS_L1,
+    OUT_L1,
+    WEIGHT_INT8_L1,
+    DIM_IM_IN_X*DIM_IM_IN_Y*CH_IM_IN,
+    CH_IM_OUT
+  );
+
+  // compute print performance
+  pi_perf_stop();          
+  int cid = pi_core_id();   
+  int perf_cyc =  pi_perf_read(PI_PERF_CYCLES);
+  int MACs = CH_IM_IN * DIM_IM_IN_X * DIM_IM_IN_Y * CH_IM_OUT;
+  float perf_MAC =  (float)MACs/perf_cyc;
+  if (cid == 0)
+  {
+    printf("\n[%d] : num_cycles: %d\n",cid,perf_cyc); 
+    printf("[%d] : MACs: %d\n",cid,MACs ); 
+    printf("[%d] : MAC/cycle: %f\n",cid,perf_MAC ); 
+    printf("[%d] : n. of Cores: %d\n",cid,NUM_CORES); 
+  }
+  pi_cl_team_barrier(0);
+
+  //check results
+  if(pi_core_id()==0)
+  {
+    for (int i=0; i<CH_IM_OUT; i++)
+    {
+      if(OUT_L1[i] != OUT_L2[i])
+      {
+        errors++;
+      }
+    }
+    printf("errors: %d\n", errors);
+  }
+  pi_cl_team_barrier(0);
+}
+
+///////////////////////////////////////////////////////////////////
+////------------------------MAIN------------------------------/////
+///////////////////////////////////////////////////////////////////
+
+int main()
+{
+  struct pi_device cluster_dev = {0};
+  struct pi_cluster_conf conf;
+  struct pi_cluster_task cluster_task = {0};
+  // task parameters allocation
+  pi_cluster_task(&cluster_task, pulp_parallel, NULL);
+  cluster_task.stack_size = 1024;
+  cluster_task.slave_stack_size = 1024;
+  // First open the cluster
+  pi_cluster_conf_init(&conf);
+  pi_open_from_conf(&cluster_dev, &conf);
+  if (pi_cluster_open(&cluster_dev))
+    return -1;
+  // Then offload an entry point, this will get executed on the cluster controller
+  pi_cluster_send_task_to_cl(&cluster_dev, &cluster_task);
+  // closing of the cluster
+  pi_cluster_close(&cluster_dev);
+
+  return 0;
+}
