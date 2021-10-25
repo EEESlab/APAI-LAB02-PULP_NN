81c94fc0acdf1bc7af788535284f9994846a08ad
Leonardo Ravaglia <leonardoravaglia94@gmail.com>
2021-10-25 17:56:47 +0200

    parallelization fixed

--- matmul_parallelization/matmuls.c
+++ matmul_parallelization/matmuls.c
@@ -3,11 +3,15 @@
 
 // generic matrix multiplication
 void gemm(int * MatA, int * MatB, int* MatC, int NN, int MM, int KK){
-    uint32_t i, core_id;
+    uint32_t i, core_id, i_chunk, i_start, i_end;
 
     core_id = pi_core_id();
+    i_chunk = NN / NUM_CORES;
+    i_start = core_id * i_chunk;
+    i_end   = i_start + i_chunk;
+
     // task to profile
-    for (i = core_id; i < NN; i += NUM_CORES) {
+    for (i = i_start; i < i_end; i += i_chunk) {
       for (int j = 0; j < MM; j++) {
         int acc = 0;
         for (int k = 0; k < KK; k++) {
